39855d322e2ff993d69ee2bdca4608f8ad9e6ac9
rohit <ghadshirohit19@gmail.com>
2024-07-11 18:42:29 +0530

    Photoshare project created

---
+++ photoshare/photoshare/photos/views.py
@@ -0,0 +1,51 @@
+from django.shortcuts import render, redirect
+
+# Create your views here.
+from .models import Category, Photo
+
+
+def gallery(request):
+    category = request.GET.get('category')
+    print('category:',category)
+    
+    if category == None:
+        photos = Photo.objects.all()
+    else:
+        photos = Photo.objects.filter(category__name=category)    
+    
+    categories = Category.objects.all()
+    # photos = Photo.objects.all()
+    context = { 'categories':categories, 'photos': photos }
+    return render(request,'photos/gallery.html', context)
+
+def viewPhoto(request, pk):
+    photo = Photo.objects.get(id = pk)
+    context = {'photo':photo}
+    return render(request,'photos/photo.html', context)
+
+def addPhoto(request):
+    categories = Category.objects.all()
+    
+    if request.method == 'POST':
+        data = request.POST
+        print(data)
+        image = request.FILES.get('image')
+        print(image)
+        
+        if data['category'] != 'none':
+            category = Category.objects.get(id= data['category'])
+        elif data['category_new'] != '':
+            category = Category.objects.get_or_create(name=data['category_new'])[0]
+        else:
+            category = None
+        
+        Photo.objects.create(
+            category = category,
+            description = data['description'],
+            image = image
+        )
+        
+        return redirect('gallery')
+    
+    context = { 'categories':categories }
+    return render(request,'photos/add.html', context)
